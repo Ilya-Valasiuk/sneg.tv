d85280f93e029476e133dc382f81bd463bc61880
Ilya-Valasiuk <ilya.valasiuk@gmail.com>
2019-03-22 00:30:58 +0300

    fix link

--- src/components/news-block/author-cologne/author-cologne-mobile.jsx
+++ src/components/news-block/author-cologne/author-cologne-mobile.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Link } from 'components/shared/link/link';
+import { Link } from 'react-router-dom';
 
 import './author-cologne.scss';
 
@@ -7,7 +7,7 @@ export function AuthorCologneMobile(props) {
   const { imgSrc, type, date, author, title } = props;
 
   return (
-    <Link className="author-cologne-mobile" href="#" style={{ backgroundImage: `url(${imgSrc})` }}>
+    <Link className="author-cologne-mobile" to="/inner" style={{ backgroundImage: `url(${imgSrc})` }}>
       <div className="author-cologne-mobile-data">
         <div className="author-cologne-mobile-date">{date}</div>
         <div className="author-cologne-mobile-type mb-3">{type}</div>
