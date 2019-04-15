a64e7d16b3098f04d380efc2a4595f393acf7153
Ilya-Valasiuk <ilya.valasiuk@gmail.com>
2019-04-15 20:11:32 +0300

    fix main menu and add tags stub data

--- src/components/tags/stub-data.js
+++ src/components/tags/stub-data.js
@@ -53,6 +53,83 @@ export const STUB_DATA = {
     values: [{
       name: 'Жизнь'
     }]
+  }, {
+    name: 'З',
+    values: [{
+      name: 'Жизнь'
+    }]
+  }, {
+    name: 'И',
+    values: []
+  }, {
+    name: 'Й',
+    values: []
+  }, {
+    name: 'К',
+    values: []
+  }, {
+    name: 'Л',
+    values: []
+  }, {
+    name: 'М',
+    values: []
+  }, {
+    name: 'Н',
+    values: []
+  }, {
+    name: 'О',
+    values: []
+  }, {
+    name: 'П',
+    values: []
+  }, {
+    name: 'Р',
+    values: []
+  }, {
+    name: 'С',
+    values: []
+  }, {
+    name: 'Т',
+    values: []
+  }, {
+    name: 'У',
+    values: []
+  }, {
+    name: 'Ф',
+    values: []
+  }, {
+    name: 'Х',
+    values: []
+  }, {
+    name: 'Ц',
+    values: []
+  }, {
+    name: 'Ч',
+    values: []
+  }, {
+    name: 'Ш',
+    values: []
+  }, {
+    name: 'Щ',
+    values: [{
+      name: 'Щука'
+    }]
+  }, {
+    name: 'Ы',
+    values: []
+  }, {
+    name: 'Э',
+    values: []
+  }, {
+    name: 'Ю',
+    values: [{
+      name: 'Юла'
+    }]
+  }, {
+    name: 'Я',
+    values: [{
+      name: 'Яблоко'
+    }]
   }],
   lettersMapEng: [{
     name: 'A',
@@ -93,6 +170,80 @@ export const STUB_DATA = {
     name: 'H',
     values: [{
       name: 'Hi'
+    }]
+  }, {
+    name: 'I',
+    values: [{
+      name: 'Iphone'
+    }, {
+      name: 'Ill'
+    }]
+  }, {
+    name: 'J',
+    values: [{
+      name: 'Jam'
+    }]
+  }, {
+    name: 'K',
+    values: []
+  }, {
+    name: 'L',
+    values: [],
+  }, {
+    name: 'M',
+    values: [{
+      name: 'Mom'
+    }]
+  }, {
+    name: 'N',
+    values: []
+  }, {
+    name: 'O',
+    values: []
+  }, {
+    name: 'P',
+    values: [{
+      name: 'Picture'
+    }]
+  }, {
+    name: 'Q',
+    values: [{
+      name: 'Queen'
+    }]
+  }, {
+    name: 'R',
+    values: []
+  }, {
+    name: 'S',
+    values: []
+  }, {
+    name: 'T',
+    values: [{
+      name: 'Table'
+    }]
+  }, {
+    name: 'U',
+    values: []
+  }, {
+    name: 'V',
+    values: [{
+      name: 'Valve'
+    }]
+  }, {
+    name: 'W',
+    values: [{
+      name: 'WoW'
+    }]
+  }, {
+    name: 'X',
+    values: []
+  }, {
+    name: 'Y',
+    values: []
+  }, {
+    name: 'Z',
+    values: [{
+      name: 'Zebro'
     }]
   }]
 };
